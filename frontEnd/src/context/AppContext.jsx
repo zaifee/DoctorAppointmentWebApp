f3f57e3a3cf24d094b02a0f1918007d12c976b2c
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-11-04 20:42:08 +0530

    Update AppContext.jsx

--- frontEnd/src/context/AppContext.jsx
+++ frontEnd/src/context/AppContext.jsx
@@ -9,7 +9,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = "https://doctorappointmentwebapp-backend.onrender.com";
     const [doctors, setDoctors] = useState([])
 
 
